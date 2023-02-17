f0cdc3170c2e9bc6502146969eb3c850eb2edda8
Kyler <kylerrosen@gmail.com>
2023-02-17 13:41:06 -0500

    bruh moment

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -19,14 +19,26 @@
   (lambda (expr state)
     (cond
       [(boolean? expr) expr]
-      [(eq? (operator expr) '&&) (and        (Mbool    (leftoperand expr) state) (Mbool (rightoperand expr) state))]
-      [(eq? (operator expr) '||) (or         (Mbool    (leftoperand expr) state) (Mbool (rightoperand expr) state))]
-      [(eq? (operator expr) '!)  (not        (Mbool    (leftoperand expr) state))]
-      [(eq? (operator expr) '==) (eq?        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '&&) (and      (Mbool    (leftoperand expr) state) (Mbool    (rightoperand expr) state))]
+      [(eq? (operator expr) '||) (or       (Mbool    (leftoperand expr) state) (Mbool    (rightoperand expr) state))]
+      [(eq? (operator expr) '!)  (not      (Mbool    (leftoperand expr) state))]
+      [(eq? (operator expr) '==) (eq?      (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
       [(eq? (operator expr) '!=) (         (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
       [(eq? (operator expr) '<)  (<        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
       [(eq? (operator expr) '>)  (>        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
-      [(eq? (operator expr) '>=) (<=        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
-      [(eq? (operator expr) '>=) (>=        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '>=) (<=       (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '>=) (>=       (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
 
       (else (error 'unknownop "Bad Operator")))))
+
+(define operator
+  (lambda (list)
+    (car list)))
+
+(define leftoperand
+  (lambda list
+    (car (cdr list))))
+
+(define rightoperand
+  (lambda list
+    (car (cdr (cdr list)))))
