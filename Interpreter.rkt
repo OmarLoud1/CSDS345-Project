27590fccc5dea09be220b67f5b7c387110037d65
Kyler <kylerrosen@gmail.com>
2023-02-17 13:36:43 -0500

    lol bool

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -10,3 +10,23 @@
       ((eq? (operator expr) '/) (quotient  (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state)))
       ((eq? (operator expr) '%) (remainder (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state)))
       (else (error 'unknownop "Bad Operator")))))
+
+(define !=
+  (lambda (expr)
+    (not (expr))))
+
+(define Mbool
+  (lambda (expr state)
+    (cond
+      [(boolean? expr) expr]
+      [(eq? (operator expr) '&&) (and        (Mbool    (leftoperand expr) state) (Mbool (rightoperand expr) state))]
+      [(eq? (operator expr) '||) (or         (Mbool    (leftoperand expr) state) (Mbool (rightoperand expr) state))]
+      [(eq? (operator expr) '!)  (not        (Mbool    (leftoperand expr) state))]
+      [(eq? (operator expr) '==) (eq?        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '!=) (         (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '<)  (<        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '>)  (>        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '>=) (<=        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+      [(eq? (operator expr) '>=) (>=        (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state))]
+
+      (else (error 'unknownop "Bad Operator")))))
