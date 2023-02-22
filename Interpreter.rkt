1161ca246f8a11d5f4defda802853415ae0abf83
OmarLoud1 <62630012+OmarLoud1@users.noreply.github.com>
2023-02-22 16:16:38 -0500

    Everything is comented

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -1,22 +1,28 @@
+;;;; ***************************************************
+;;;; Group 6: Omar Loudghiri(oxl51), Kyler Rosen(kkr33), Niels Sogaard(nks69)
+;;;; CSDS 345 Spring 2023
+;;;; Project 1
+;;;; ***************************************************
+
+;Basic Interpreter
 #lang racket
 
 (require "simpleParser.rkt")
+;;;; ***************************************************
+;Helper functions to parse the lists of expressions
+;;;; ***************************************************
 
-(define intrepret
-  (lambda (file)
-    (M_statement (parser file) '(()()) )
-  )
-)
-
-; splitting expr
+;gets the operator in an expression
 (define operator
   (lambda (list)
     (car list)))
 
+;gets the operand in an expression
 (define operand
   (lambda (list)
     (car (cdr list))))
 
+;gets the nth operand in a multi-operand expression
 (define operandn
   (lambda (n list)
     (cond
@@ -24,29 +30,37 @@
      ((zero? n) (car list))
      (else (operandn (- n 1) (cdr list))))))
 
+;gets the left operand in prefix notation
 (define leftoperand
   (lambda (list)
     (car (cdr list))))
 
+;gets the right operand
 (define rightoperand
   (lambda (list)
     (if (or (null? (cdr list)) (null? (cdr (cdr list))))
         '()
         (car (cdr (cdr list))))))
 
-(define vars
-  (lambda (state)
-    (car state)))
-
-(define vals
-  (lambda (state)
-    (car (cdr state))))
-
+;gets the parameters of an expression
 (define parameters
   (lambda (expr)
     (cdr expr)))
 
-(provide Minteger)
+;gets the variable name from the state list
+(define vars
+  (lambda (state)
+    (car state)))
+
+;gets the value of the variable from the list
+(define vals
+  (lambda (state)
+    (car (cdr state))))
+
+;;;; ***************************************************
+; Main functions to parse the statements
+;;;; ***************************************************
+
 ; Finds the integer value of an expression
 (define Minteger
   (lambda (expr state)
@@ -61,13 +75,9 @@
       ((eq? (operator expr) '%) (remainder (Minteger (leftoperand expr) state) (Minteger (rightoperand expr) state)))
       (else (error 'unknownop "Bad Operator"))))) 
 
-; tests if two expressions are not equal
-(define neq?
-  (lambda (expr1 expr2)
-    (not (eq? expr1 expr2))))
+
 
 ; Finds the boolean value of an expression
-(provide Mbool)
 (define Mbool
   (lambda (expr state)
     (cond
@@ -88,23 +98,9 @@
 
       (else (error 'unknownop "Bad Operator")))))
 
-; checks if a variable is in a list
-(define member?
-  (lambda (x list)
-    (cond
-      ((null? list) #f)
-      ((eq? x (car list)) #t)
-      (else (member? x (cdr list))))))
 
-; checks if a variable is initialized
-(define initialized?
-  (lambda (var state)
-    (member? var (car state))))
-
-
-(provide getState)
 ; Gets the value of a variable
-(define getState
+(define MgetState
   (lambda (varName state)
     (cond
       [(or (null? (car state)) (null? (car (cdr state)))) (error 'gStateError "There was a problem finding that variable.")]
@@ -116,31 +112,9 @@
   )
 )  
 
-; replaces a variable with a value in a list
-(define replace
-  (lambda (x y lis)
-    (cond
-      [(null? lis) '()]
-      [(eq? (car lis) x) (cons y (replace x y (cdr lis)))]
-      [else (cons (car lis) (replace x y (cdr lis)))])))
-
-; this is currently not being used?
-; removes a variable from a list
-(define remove
-  (lambda (var lis)
-    (cond 
-      [(null? lis) '()]
-      [(eq? (car lis) var) (cdr lis)]
-      [else (cons (car lis) (remove var (cdr lis)))])))
-
-;; checks if a variable is declared
-(define declared?
-  (lambda (var state)
-    (member? var (vars state))))
-
 
 ; delares a variable
-(define M_declare
+(define Mdeclare
   (lambda (var val state)
     (if (null? val)
         (StateUpdate (list var '$null$) state)
@@ -148,7 +122,7 @@
 
 
 ; assigns a value to a variable
-(define M_assign
+(define Massign
   (lambda (var val state)
     (if (declared? var state)
       (StateUpdate (list var (Mval val state)) state)
@@ -165,7 +139,7 @@
 
 
 ; executes an if expression given a condition, an expression to execute if true, an expression to execute if false, and the state
-(define M_if
+(define Mif
   (lambda (condition expr exprelse state)
     (cond
       [(eq? (Mbool condition state) #t)     (StateUpdate (M_statement expr state) state)]
@@ -174,13 +148,13 @@
 
 
 ; executes a while loop given a condition, an expression to execute while true, and the state
-(define M_while
+(define Mwhile
   (lambda (condition expr state)
     (cond
       [(Mbool condition state) (M_while condition expr (StateUpdate (M_statement expr state) state))]
       [else state])))
 
-(define M_return
+(define Mreturn
   (lambda (expr state)
     (cond
       [(eq? (M_statement expr state) #t)      (StateUpdate (list 'return "true") state)]
@@ -189,7 +163,7 @@
 
 
 ; determines the type of statement and executes its function
-(define M_statement
+(define Mstatement
   (lambda (expr state)
     (cond
       [(intexp? expr state)                                                       (Minteger expr state)]
@@ -201,7 +175,50 @@
       [(eq? (operator expr) 'while)              (M_while (leftoperand expr) (rightoperand expr) state)]
       [else (error 'unknownop "Bad Statement")])))
 
+; iterates across statement list executing expressions
+(define Mstatementlist
+  (lambda (expr state)
+    (if (null? (cdr expr))
+        (getState 'return (M_statement (car expr) state))
+        (M_statementlist (cdr expr) (M_statement (car expr) state)))))
 
+
+; checks if a variable is initialized
+(define initialized?
+  (lambda (var state)
+    (member? var (car state))))
+
+
+; adds the declared variable to the state, removes past instance of it
+(define addState
+  (lambda (declared state)
+    (list (cons (car declared) (vars state)) (cons (vals declared) (vals state)))))
+
+
+; updates status given a declared variable
+(define StateUpdate
+  (lambda (declared state)
+    (cond
+      [(or (null? declared) (null? (car declared))) state]
+      [(list? (vars declared)) (StateUpdate (list (car (vars declared)) (car (vals declared))) (StateUpdate (list (cdr (vars declared)) (cdr (vals declared))) state))]
+      [else (addState declared (removevar declared state))])))
+
+ ; cps helper function for remove  
+(define removevar-cps
+  (lambda (declared statevars statevals return)
+    (cond
+      [(null? statevars)                                                                        (return '() '())]
+      [(eq? (car statevars) (car declared))                             (return (cdr statevars) (cdr statevals))]
+      [else (removevar-cps declared (cdr statevars) (cdr statevals) 
+                                  (lambda (v1 v2) (return (cons (car statevars) v1) (cons (car statevals) v2))))])))
+
+
+; removes the declared variable from the state
+(define removevar
+  (lambda (declared state)
+    (removevar-cps declared (car state) (cadr state) (lambda (v1 v2) (list v1 v2)))))
+
+;checks whether an expression is an integer expression
 (define intexp?
   (lambda (expr state)
     (cond
@@ -233,50 +250,56 @@
       [(eq? (operator expr) '>=) #t]
 
       (else #f))))
-    
 
 
-(provide removevar-cps)
 
- ; cps helper function for remove   
-(define removevar-cps
-  (lambda (declared statevars statevals return)
-    (cond
-      [(null? statevars)                                                                        (return '() '())]
-      [(eq? (car statevars) (car declared))                             (return (cdr statevars) (cdr statevals))]
-      [else (removevar-cps declared (cdr statevars) (cdr statevals) 
-                                  (lambda (v1 v2) (return (cons (car statevars) v1) (cons (car statevals) v2))))])))
-
-(provide removevar)
-; removes the declared variable from the state
-(define removevar
-  (lambda (declared state)
-    (removevar-cps declared (car state) (cadr state) (lambda (v1 v2) (list v1 v2)))))
-
-; adds the declared variable to the state, removes past instance of it
-(define addState
-  (lambda (declared state)
-    (list (cons (car declared) (vars state)) (cons (vals declared) (vals state)))))
-
-(provide StateUpdate)
-
-; updates status given a declared variable
-(define StateUpdate
-  (lambda (declared state)
-    (cond
-      [(or (null? declared) (null? (car declared))) state]
-      [(list? (vars declared)) (StateUpdate (list (car (vars declared)) (car (vals declared))) (StateUpdate (list (cdr (vars declared)) (cdr (vals declared))) state))]
-      [else (addState declared (removevar declared state))])))
-
-; iterates across statement list executing expressions
-(define M_statementlist
-  (lambda (expr state)
-    (if (null? (cdr expr))
-        (getState 'return (M_statement (car expr) state))
-        (M_statementlist (cdr expr) (M_statement (car expr) state)))))
-
+;;;; ***************************************************
+; Main method
+;;;; ***************************************************
 (define run
   (lambda (expr)
     (M_statementlist expr '(() ()))))
 
 (run (parser "tests/test15.txt"))
+
+
+
+
+;;;; ***************************************************
+; Auxiliary helper methods from class
+;;;; ***************************************************
+
+; tests if two expressions are not equal
+(define neq?
+  (lambda (expr1 expr2)
+    (not (eq? expr1 expr2))))
+
+; checks if a variable is in a list
+(define member?
+  (lambda (x list)
+    (cond
+      ((null? list) #f)
+      ((eq? x (car list)) #t)
+      (else (member? x (cdr list))))))
+
+; replaces a variable with a value in a list
+(define replace
+  (lambda (x y lis)
+    (cond
+      [(null? lis) '()]
+      [(eq? (car lis) x) (cons y (replace x y (cdr lis)))]
+      [else (cons (car lis) (replace x y (cdr lis)))])))
+
+; this is currently not being used?
+; removes a variable from a list
+(define remove
+  (lambda (var lis)
+    (cond 
+      [(null? lis) '()]
+      [(eq? (car lis) var) (cdr lis)]
+      [else (cons (car lis) (remove var (cdr lis)))])))
+
+; checks if a variable is declared
+(define declared?
+  (lambda (var state)
+    (member? var (vars state))))
